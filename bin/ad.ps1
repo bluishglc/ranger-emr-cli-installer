392a80cdd5c4bd6e9e64cc7ae55859689513e3a3
buishglc <buishglc@126.com>
2022-08-01 16:40:58 +0800

    refactor AD installation scripts.

--- bin/ad.ps1
+++ bin/ad.ps1
@@ -2,6 +2,7 @@
 $WebClient = New-Object System.Net.WebClient
 $WebClient.DownloadFile("https://raw.githubusercontent.com/bluishglc/ranger-emr-cli-installer/master/bin/ad-1.ps1","ad-1.ps1")
 $WebClient.DownloadFile("https://raw.githubusercontent.com/bluishglc/ranger-emr-cli-installer/master/bin/ad-2.ps1","ad-2.ps1")
+$WebClient.DownloadFile("https://raw.githubusercontent.com/bluishglc/ranger-emr-cli-installer/master/conf/ad/kerberos-encrytion-types.reg","kerberos-encrytion-types.reg")
 
 workflow Install-AD {
     ad-1.ps1
