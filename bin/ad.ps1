0c3e8c3cad1e7c9a4136ae00e4c6ba14eb6b4799
buishglc <buishglc@126.com>
2022-08-01 16:35:24 +0800

    refactor AD installation scripts.

--- bin/ad.ps1
+++ bin/ad.ps1
@@ -1,63 +1,14 @@
-# install Active Directory
-Install-windowsfeature -name AD-Domain-Services -IncludeManagementTools
 
-# create forest, this command require reboot, although -NoRebootOnCompletion:$true can help skip reboot
-# however, it is still needed for AD to reboot anyway.
-Install-ADDSForest -DomainName example.com -SafeModeAdministratorPassword (ConvertTo-SecureString 'Admin1234!' -AsPlainText -Force) -DomainMode WinThreshold -DomainNetbiosName ABC -ForestMode WinThreshold -DatabasePath "C:\Windows\NTDS" -LogPath "C:\Windows\NTDS" -SysvolPath "C:\Windows\SYSVOL" -CreateDnsDelegation:$false -InstallDns:$true -NoRebootOnCompletion:$false -Force:$true
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile("https://raw.githubusercontent.com/bluishglc/ranger-emr-cli-installer/master/bin/ad-1.ps1","ad-1.ps1")
 
-ksetup /addkdc CN-NORTH-1.COMPUTE.INTERNAL
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile("https://raw.githubusercontent.com/bluishglc/ranger-emr-cli-installer/master/bin/ad-2.ps1","ad-2.ps1")
 
-netdom trust CN-NORTH-1.COMPUTE.INTERNAL /Domain:EXAMPLE.COM /add /realm /passwordt:Admin1234!
+workflow Install-AD {
+    ad-1.ps1
+    Restart-Computer -Wait
+    ad-2.ps1
+}
 
-# need config "encrypt types for Kerberos" on AD (it seems UI only),
-# add one or both following items:
-ksetup /SetEncTypeAttr CN-NORTH-1.COMPUTE.INTERNAL AES256-CTS-HMAC-SHA1-96 AES128-CTS-HMAC-SHA1-96
-
-# ------------------------------------------   ou: Service Accounts   ------------------------------------------- #
-
-# Remove OU "Service Accounts" recursively if exists
-# Get-ADOrganizationalUnit -Identity "OU=Service Accounts,DC=EXAMPLE,DC=COM" |
-# Set-ADObject -ProtectedFromAccidentalDeletion:$false -PassThru |
-# Remove-ADOrganizationalUnit -Confirm:$false -Recursive
-
-# Add OU "Service Accounts"
-New-ADOrganizationalUnit -Name "Service Accounts" -Path "DC=EXAMPLE,DC=COM"
-
-# -------------------------------------------   user: ranger-binder   ------------------------------------------- #
-
-# Remove service account "ranger-binder" if exists
-Remove-ADUser -Identity "CN=ranger-binder,OU=Service Accounts,DC=EXAMPLE,DC=COM" -Confirm:$false
-
-# Create service account "ranger-binder"
-New-ADUser -Name "ranger-binder" -OtherAttributes @{'title'="ranger-binder"} -path "OU=Service Accounts,DC=EXAMPLE,DC=COM"
-Set-ADAccountPassword -Identity "CN=ranger-binder,OU=Service Accounts,DC=EXAMPLE,DC=COM" -Reset -NewPassword (ConvertTo-SecureString -AsPlainText 'Admin1234!' -Force)
-Set-ADUser -Identity "CN=ranger-binder,OU=Service Accounts,DC=EXAMPLE,DC=COM" -PasswordNeverExpires $true
-Enable-ADAccount -Identity "CN=ranger-binder,OU=Service Accounts,DC=EXAMPLE,DC=COM"
-
-# ----------------------------------------   user: emr-ad-domain-joiner   --------------------------------------- #
-
-# Remove service account "emr-ad-domain-joiner" if exists
-Remove-ADUser -Identity "CN=emr-ad-domain-joiner,OU=Service Accounts,DC=EXAMPLE,DC=COM" -Confirm:$false
-
-# Create service account "emr-ad-domain-joiner"
-New-ADUser -Name "emr-ad-domain-joiner" -OtherAttributes @{'title'="emr-ad-domain-joiner"} -path "OU=Service Accounts,DC=EXAMPLE,DC=COM"
-Set-ADAccountPassword -Identity "CN=emr-ad-domain-joiner,OU=Service Accounts,DC=EXAMPLE,DC=COM" -Reset -NewPassword (ConvertTo-SecureString -AsPlainText 'Admin1234!' -Force)
-Set-ADUser -Identity "CN=emr-ad-domain-joiner,OU=Service Accounts,DC=EXAMPLE,DC=COM" -PasswordNeverExpires $true
-Enable-ADAccount -Identity "CN=emr-ad-domain-joiner,OU=Service Accounts,DC=EXAMPLE,DC=COM"
-
-# Add service account "emr-ad-domain-joiner" to "Domain Admins", "Schema Admins", "Enterprise Admins" groups,
-# so as it has enough privileges to add EMR cluster nodes into AD domain.
-Add-ADGroupMember -Identity "Domain Admins" -Members "CN=emr-ad-domain-joiner,OU=Service Accounts,DC=EXAMPLE,DC=COM"
-Add-ADGroupMember -Identity "Schema Admins" -Members "CN=emr-ad-domain-joiner,OU=Service Accounts,DC=EXAMPLE,DC=COM"
-Add-ADGroupMember -Identity "Enterprise Admins" -Members "CN=emr-ad-domain-joiner,OU=Service Accounts,DC=EXAMPLE,DC=COM"
-
-# ---------------------------------------------   user: ad-user-1   --------------------------------------------- #
-
-# Remove service account "emr-ad-domain-joiner" if exists
-Remove-ADUser -Identity "CN=ad-user-1,CN=Users,DC=EXAMPLE,DC=COM" -Confirm:$false
-
-# Create normal domain user  "ad-user-1"
-New-ADUser -Name "ad-user-1" -OtherAttributes @{'title'="ad-user-1"} -path "CN=Users,DC=EXAMPLE,DC=COM"
-Set-ADAccountPassword -Identity "CN=ad-user-1,CN=Users,DC=EXAMPLE,DC=COM" -Reset -NewPassword (ConvertTo-SecureString -AsPlainText 'Admin1234!' -Force)
-Set-ADUser -Identity "CN=ad-user-1,CN=Users,DC=EXAMPLE,DC=COM" -PasswordNeverExpires $true
-Enable-ADAccount -Identity "CN=ad-user-1,CN=Users,DC=EXAMPLE,DC=COM"
+Install-AD
